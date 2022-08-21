d6288f4ff3a477e7fe14675e990d8bb6c67ac642
Daniel Españadero <developer@daniel-espanadero.com>
2022-08-21 19:54:22 +0200

    Window created

--- src/game/Snake.java
+++ src/game/Snake.java
@@ -1,7 +1,21 @@
 package game;
 
-public class Snake {
+import javax.swing.*;
+
+public class Snake extends JFrame {
 	
+	int width = 640;
+	int height= 480;
+	
+	public Snake() {
+		setTitle("Snake");
+		
+		setSize(width, height);
+		
+		setVisible(true);
+	}
+	
+//	Run | Debug
 	public static void main(String[] args) {
 		Snake s = new Snake();
 	}
