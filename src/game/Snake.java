8c739d77dd33e82afb25176d7bdc84e910042896
Daniel Españadero <developer@daniel-espanadero.com>
2022-08-21 20:42:26 +0200

    Image snake created

--- src/game/Snake.java
+++ src/game/Snake.java
@@ -3,14 +3,23 @@ package game;
 import javax.swing.*;
 
 import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.event.*;
 import java.awt.Toolkit;
 import java.awt.Point;
+import java.awt.Color;
+import java.awt.Graphics;
 
 public class Snake extends JFrame {
 	
 	int width = 640;
 	int height= 480;
+	
+	Point snake;
+	int widthPoint = 10;
+	int heightPoint = 10;
+	
+	ImageSnake imageSnake = new ImageSnake();
 	
 	public Snake() {
 		setTitle("Snake");
@@ -28,12 +37,25 @@ public class Snake extends JFrame {
 		Teclas teclas = new Teclas();
 		this.addKeyListener(teclas);
 		
+		snake = new Point(width/2, height/2);
+		
+		imageSnake = new ImageSnake();
+		this.getContentPane().add(imageSnake);
 		setVisible(true);
 	}
 	
 //	Run | Debug
 	public static void main(String[] args) {
 		Snake s = new Snake();
+	}
+	
+	public class ImageSnake extends JPanel {
+		public void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			
+			g.setColor(new Color(0, 0, 255));
+			g.drawRect(snake.x, snake.y, widthPoint, heightPoint);
+		}
 	}
 	
 	public class Teclas extends KeyAdapter{
